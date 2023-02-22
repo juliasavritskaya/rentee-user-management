b7e739396a309a581311cea55a90a0f83d32fb20
juliasavritskaya <mnkqn0211>
2023-02-22 04:29:49 +0400

    Initial commit

---
+++ src/main/java/com/mnkqn/renteeusermanagement/RenteeUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mnkqn.renteeusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenteeUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenteeUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnkqn/renteeusermanagement/RenteeUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnkqn.renteeusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenteeUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
